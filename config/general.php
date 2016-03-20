45306ab808a55b0cef79897a433b478f717c2b73
Barrett Alexander <barrett@clockwork.net>
2016-03-20 17:41:48 -0500

    Added default config settings

--- config/general.php
+++ config/general.php
@@ -7,6 +7,26 @@
  * You can see a list of the default settings in craft/app/etc/config/defaults/general.php
  */
 
-return array(
+return [
+    '*' => [
+        'autoLoginAfterAccountActivation' => true,
+        'csrfTokenName' => 'csrf',
+        'devMode' => false,
+        'enableCsrfProtection' => true,
+        'environmentVariables' => [
+            'baseUrl' => '',
+            'basePath' => '',
+        ],
+        'omitScriptNameInUrls' => true,
+    ],
 
-);
+    'vagrant.local' => [
+        'devMode' => true,
+        'backupDbOnUpdate' => false,
+        'restoreDbOnUpdateFailure' => false,
+        'environmentVariables' => [
+            'baseUrl' => 'http://localhost:8080',
+            'basePath' => '/vagrant/public',
+        ],
+    ]
+];
