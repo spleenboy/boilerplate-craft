30d9cf169a7c11e7d4ee1e0b7ceef6cca2e45638
Barrett Alexander <barrett@clockwork.net>
2016-04-04 14:52:58 -0500

    Added more verbosity to vagrant script

--- bin/vagrant.sh
+++ bin/vagrant.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
+
 if [ -n "$1" ]
 then
-    echo "vagrant ssh -c '/vagrant/$1'"
-
-    if [ "$2" != "-d" ]
+    if [ -f "$1" ]
     then
-        vagrant ssh -c "/vagrant/$1"
+        echo "vagrant ssh -c '/vagrant/$1'"
+
+        if [ "$2" != "-d" ]
+        then
+            vagrant ssh -c "/vagrant/$1"
+        fi
+    else
+        echo "$1 is not a valid file."
     fi
 else
     echo "Passes command to running vagrant instance."
+    echo
     echo "Usage:"
-    echo "bin/vagrant.sh <LOCAL_PATH_TO_SCRIPT>"
+    echo " bin/vagrant.sh <LOCAL_PATH_TO_SCRIPT>"
+    echo
+    echo "Flags:"
+    echo " -d  Dry run"
 fi
